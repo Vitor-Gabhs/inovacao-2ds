b62c4a2a874f2dd9923c879c06f9edbdea79f62f
Vitor-Gabhs <vitor.dutra.marques@escola.pr.gov.br>
2026-08-19 15:46:30 -0300

    Create  triondaBateria.py

---
+++ triondaBateria.py
@@ -0,0 +1,9 @@
+bateria_atual = 
+bola_em_jogo = 
+
+if bateria_atual <= 15  and bola_em_jogo == True:
+    print("ALERTA MÁXIMO: Bateria baixa! Substitua a bola na próxima paralisação.")
+elif bateria_atual <= 15  and bola_em_jogo == False:
+    print("Aviso: Bateria baixa. Aproveite a bola parada para trocá-la.")
+else:
+    print("Sistema Trionda operando normalmente. Bateria ok.")
